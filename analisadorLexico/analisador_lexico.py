440a8959f64b596b386fc1affc4f9b58c29b5d94
Andre Luiz Branco Barbosa Araujo <andreluizpsn975@hotmail.com>
2020-11-09 00:09:05 -0300

    repassando linha e coluna de cada lexema

--- analisadorLexico/analisador_lexico.py
+++ analisadorLexico/analisador_lexico.py
@@ -57,11 +57,12 @@ class LexicalAnalyzer:
         self.symbleTable.addSymbol("lit","lit","")
         self.symbleTable.addSymbol("real","real","")
 
-    def lexicon(self, lexeme, token):
+    def lexicon(self, lexeme, token, line, column):
         if self.symbleTable.checkSymbolExistence(lexeme):
-            self.symbols.append(self.symbleTable.symbol[lexeme])
+            symbol = self.symbleTable.symbol[lexeme]
+            self.symbols.append({"lexema": symbol["lexema"], "token": symbol["token"], "tipo": symbol["tipo"], "line": line, "column": column})
         else:
-            self.symbols.append({"lexema": lexeme , "token": token , "tipo": "" })
+            self.symbols.append({"lexema": lexeme, "token": token, "tipo": "", "line": line, "column": column})
 
     def readFile(self , fileName):
         return open(fileName,"r") #Lê o arquivo indicado
@@ -81,19 +82,19 @@ class LexicalAnalyzer:
                     if character == " "  or character == "\n": #Determinando as condições para para de ler um lexema e registrar ele
                         if erro != "": #Caso já esteja registrando um falso lexema continua até ele terminar
                             self.errors.append([erro,lineNumber, columnNumber, currentState[1]])
-                            self.lexicon(erro, state[1])
+                            self.lexicon(erro, state[1], lineNumber, columnNumber)
                             erro = ""
                         elif self.DFA.isValidFinalState(state[0]): #Registra os lexemas
                             if state[1] == "id":
                                 self.symbleTable.addSymbol(lexema, state[1], "")
-                            self.lexicon(lexema, state[1])
+                            self.lexicon(lexema, state[1], lineNumber, columnNumber)
                         state = initialState
                         lexema = ""
                     elif specialState[0] != "Se" and currentState[1] != "1":#Permite que seja salvo um lexema logo após outro sem um espaço em branco
                         #print("\n\n" + lexema + "\n\n")
                         if state[1] == "id":
                             self.symbleTable.addSymbol(lexema, state[1], "")
-                        self.lexicon(lexema, state[1])
+                        self.lexicon(lexema, state[1], lineNumber, columnNumber)
                         state = specialState
                         lexema = character
                     else: #Não para de registrar qdo encontra um simbolo inválido, mas guarda ele separadamente
@@ -103,17 +104,17 @@ class LexicalAnalyzer:
                 elif state[0] == "s8" or state[0] == "s12": #O estado descrito aqui adiciona um erro caso a linha termine sem fechar aspas
                     if state[0] == "s8" and character == '"':
                         lexema =  lexema + character
-                        self.lexicon(lexema, currentState[1])
+                        self.lexicon(lexema, currentState[1], lineNumber, columnNumber)
                         state = initialState
                         lexema = ""
                     elif state[0] == "s12" and character == "}":
                         lexema =  lexema + character
-                        self.lexicon(lexema, currentState[1])
+                        self.lexicon(lexema, currentState[1], lineNumber, columnNumber)
                         state = initialState
                         lexema = ""
                     elif character == "\n" or character == EOFError:
                         self.errors.append([lexema,lineNumber, columnNumber, currentState[1]])
-                        self.lexicon(lexema, state[1])
+                        self.lexicon(lexema, state[1], lineNumber, columnNumber)
                         lexema = ""
                         state = initialState
                     else:
@@ -128,12 +129,12 @@ class LexicalAnalyzer:
             if self.DFA.isValidFinalState(state[0]):
                 if state[1] == 'id':
                     self.symbleTable.addSymbol(lexema, state[1], "")
-                self.lexicon(lexema, state[1])
+                self.lexicon(lexema, state[1], lineNumber, columnNumber)
             else:
                 if state[0] == "s8" or state[0] == "s12":
                     self.errors.append([lexema,lineNumber, columnNumber, currentState[1]])
-                    self.lexicon(lexema, state[1])
+                    self.lexicon(lexema, state[1], lineNumber, columnNumber)
                 else:
                     self.errors.append([erro,lineNumber, columnNumber, currentState[1]])
-                    self.lexicon(erro, state[1])
-        self.lexicon("","$")
+                    self.lexicon(erro, state[1], lineNumber, columnNumber)
+        self.lexicon("","$", lineNumber + 1 , columnNumber + 1)
